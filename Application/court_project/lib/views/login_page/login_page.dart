3ccf6029ab2e78cf44f3418a0816c398abf4bed9
Sri Raja Vignesh.S <srirajarox@gmail.com>
2022-05-02 01:14:05 +0530

    added login logic

--- Application/court_project/lib/views/login_page/login_page.dart
+++ Application/court_project/lib/views/login_page/login_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:court_project/utils/nav_bar.dart';
+import 'package:court_project/views/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,9 +16,46 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  void login() {
-    debugPrint("username: ${_usernameController.text}");
-    debugPrint("password: ${_passwordController.text}");
+  void login() async {
+    String _username = _usernameController.text;
+    String _password = _passwordController.text;
+
+    try {
+      var response = await http.post(
+          Uri.parse("http://127.0.0.1/cms/get_user.php"),
+          body: {'username': _username, 'password': _password});
+      List<dynamic> fetchedData = jsonDecode(response.body) as List<dynamic>;
+
+      if (fetchedData.isNotEmpty) {
+        String name = fetchedData[0]["name"];
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Logged in, welcome $name"),
+            backgroundColor: Colors.green,
+          ),
+        );
+        Navigator.push(context,
+            MaterialPageRoute(builder: ((context) => const HomePage())));
+      } else {
+        showDialog(
+          context: context,
+          builder: (_) => const AlertDialog(
+            title: Text("Invalid email/password"),
+            content: Text("The username/password entered is incorrect"),
+          ),
+        );
+        return;
+      }
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (_) => const AlertDialog(
+          title: Text("Something went wrong"),
+          content: Text("Some problem occured with the server."),
+        ),
+      );
+      return;
+    }
   }
 
   @override
@@ -103,6 +144,9 @@ class _LoginPageState extends State<LoginPage> {
                     child: TextField(
                       controller: _passwordController,
                       cursorColor: Colors.black,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
                       decoration: const InputDecoration(
                         hintText: "Password",
                         fillColor: Colors.white,
@@ -122,7 +166,9 @@ class _LoginPageState extends State<LoginPage> {
                     width: width * 0.4 * 0.3,
                     height: height * 0.56 * 0.115,
                     child: ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          login();
+                        },
                         style: ButtonStyle(
                           backgroundColor: MaterialStateProperty.all(
                             const Color.fromRGBO(18, 41, 76, 1),
